ea37fd86ed07d49b86f9eafe3e1568647401b111
Logicae <justin.gore@outlook.com>
2017-02-08 14:17:52 -0500

    added config and index methods

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,14 @@
 class UserController < ApplicationController
-end 
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+end
