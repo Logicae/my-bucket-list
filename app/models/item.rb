97e2e1b191c17eab2900983be9c92b2775ed123e
Logicae <justin.gore@outlook.com>
2017-02-08 14:07:42 -0500

    defined belongs_to relationship

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,4 @@
 class Item < ActiveRecord::Base
   belongs_to :user
-
-  has_secure_password
-
-  def slug
-    self.username.downcase.strip.gsub(' ', '-')
-  end
-
-  def self.find_by_slug(slug)
-    self.all.each do |s|
-      if s.slug == slug
-        return s
-      end
-    end
-  end
+  
 end
