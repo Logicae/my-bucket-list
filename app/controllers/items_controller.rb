7c11dd1bf498f939afc7b9cdbda078a26a1f248f
Logicae <justin.gore@outlook.com>
2017-02-08 22:51:26 -0500

    added post action and action to render individual item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,4 +16,24 @@ class ItemsController < ApplicationController
       redirect to "/login"
     end
   end
+
+  post '/items' do
+    if params[:title] == "" || params[:content] == ""
+      redirect to "/items/new"
+    else
+      @item = Item.create(params)
+      @user = current_user
+      @user.items << @item
+      redirect to "/items/#{@item.id}"
+    end
+  end
+
+  get '/items/:id' do
+    if logged_in?
+      @item = Item.find(params[:id])
+      erb :"items/show_item"
+    else
+      redirect to "/login"
+    end
+  end
 end
